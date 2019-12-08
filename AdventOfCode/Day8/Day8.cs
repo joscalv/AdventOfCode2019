e2afebb274c1daf84af448250c2a384fea243178
joscalv <joscalv@gmail.com>
2019-12-08 13:00:01 +0100

    Day 8

---
+++ AdventOfCode/Day8/Day8.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Text;
+
+namespace AdventOfCode.Day8
+{
+    public static class Day8
+    {
+        public static int Part1()
+        {
+            var input = ReadInput();
+            var imageBytes = ImageDecoder.StringToIntArray(input);
+            int width = 25;
+            int height = 6;
+            var image = new Image(width, height, imageBytes);
+
+            int zeros = Int32.MaxValue;
+            int unoes = 0;
+            int two = 0;
+            var tmp = 0;
+            for (int i = 0; i < image.Layers.Length; i++)
+            {
+                tmp = NumberOf(image.Layers[i], 0);
+                if (tmp < zeros)
+                {
+                    zeros = tmp;
+                    unoes = NumberOf(image.Layers[i], 1);
+                    two = NumberOf(image.Layers[i], 2);
+                }
+            }
+
+            return unoes * two;
+        }
+
+        public static void Part2()
+        {
+            var input = ReadInput();
+            var imageBytes = ImageDecoder.StringToIntArray(input);
+            int width = 25;
+            int height = 6;
+            var image = new Image(width, height, imageBytes);
+
+            var result = image.DecodeImage();
+
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    if (x == 0)
+                    {
+                        Console.Write(Environment.NewLine);
+                    }
+                    Console.Write((char)(result[y,x]));
+                    
+                }
+            }
+        }
+
+        private static int NumberOf(int[][] dataLayer, int value)
+        {
+            return dataLayer.SelectMany(v => v).Count(v => v == value);
+        }
+
+
+        private static string ReadInput()
+        {
+            return File.ReadAllText(@"Inputs\inputDay8.txt");
+        }
+
+
+    }
+
+    public static class ImageDecoder
+    {
+        public static int[] StringToIntArray(string s)
+        {
+            int length = s.Length;
+            int[] result = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = s[i] - 48;
+            }
+            return result;
+        }
+
+
+        public static int[,] DecodeImage(this Image img)
+        {
+            var result = new int[img.Height, img.Width];
+            var colors = new int[] {32, 35, 0};
+
+            for (int layer = 0; layer < img.Layers.Length; layer++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    for (int y = 0; y < img.Height; y++)
+                    {
+                        if (result[y,x]== 0)
+                        {
+                            result[y, x] = colors[img.Layers[layer][y][x]];
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class Image
+    {
+        public int Width { get; }
+        public int Height { get; }
+        private readonly int[] _data;
+
+        public Image(int width, int height, int[] data)
+        {
+            Width = width;
+            Height = height;
+            _data = data;
+            var layerSize = Width * Height;
+            var numLayers = data.Length / layerSize;
+            var layerData = new int[numLayers][][];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int layer = i / layerSize;
+                int elementInLayer = i % layerSize;
+                if (elementInLayer == 0)
+                {
+                    layerData[layer] = new int[height][];
+                }
+
+                int elementInRow = elementInLayer % width;
+                int row = elementInLayer / width;
+                if (elementInRow == 0)
+                {
+                    layerData[layer][row] = new int[width];
+                }
+                layerData[layer][row][elementInRow] = data[i];
+            }
+
+            Layers = layerData;
+        }
+
+        public int[][][] Layers { get; }
+    }
+}
+
---
+++ AdventOfCode2019Test/Day8Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AdventOfCode.Day8;
+using Xunit;
+
+namespace AdventOfCode2019Test
+{
+    public class Day8Test
+    {
+        [Fact]
+        public void Part1Test()
+        {
+
+            var value = Day8.Part1();
+
+            Assert.Equal(1215, value);
+
+        }
+
+        [Fact]
+        public void DecodeTest()
+        {
+            string image = "123456789012";
+            var data = ImageDecoder.StringToIntArray(image);
+            var img = new Image(3, 2, data);
+            Assert.Equal(new int[] { 1, 2, 3 }, img.Layers[0][0]);
+            Assert.Equal(new int[] { 4, 5, 6 }, img.Layers[0][1]);
+            Assert.Equal(new int[] { 7, 8, 9 }, img.Layers[1][0]);
+            Assert.Equal(new int[] { 0, 1, 2 }, img.Layers[1][1]);
+        }
+
+        [Fact]
+        public void StringToIntTest()
+        {
+            string image = "123456789012";
+            int[] expected = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2 };
+
+            var result = ImageDecoder.StringToIntArray(image);
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
