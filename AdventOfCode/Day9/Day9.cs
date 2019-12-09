9f30892b13353dfb9f5ebce1f740a1131dda8583
joscalv <joscalv@gmail.com>
2019-12-09 12:54:47 +0100

    - Remove copied code

---
+++ AdventOfCode/Day9/Day9.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Linq;
+using AdventOfCode.IntCode;
+using AdventOfCode.IntCode.IO;
+
+namespace AdventOfCode.Day9
+{
+    public class Day9
+    {
+        private static readonly long[] Program = ReadInput();
+
+        public static void Init()
+        {
+        }
+
+        public static long Part1()
+        {
+            var outputConsole = new OutputFixed();
+            var computer = new IntCodeComputer(Program, new InputFixedValue(1), outputConsole);
+            var result = computer.Execute();
+
+            return outputConsole.GetOutPut();
+        }
+
+        public static long Part2()
+        {
+            var outputConsole = new OutputFixed();
+            var computer = new IntCodeComputer(Program, new InputFixedValue(2), outputConsole);
+            var result = computer.Execute();
+
+            return outputConsole.GetOutPut();
+        }
+
+        private static long[] ReadInput()
+        {
+            var program = File
+                .ReadAllText(@"Inputs\inputDay9.txt")
+                .Split(',')
+                .Select(long.Parse).ToArray();
+            return program;
+        }
+    }
+}
