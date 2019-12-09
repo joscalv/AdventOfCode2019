06bedc149ed31d165ed054979c822e5ab1350792
joscalv <joscalv@gmail.com>
2019-12-09 13:18:26 +0100

    Order day 5 Test

--- AdventOfCode2019Test/Day5Test.cs
+++ AdventOfCode2019Test/Day5Test.cs
@@ -1,4 +1,6 @@
 ﻿using AdventOfCode.Day5;
+using AdventOfCode.IntCode;
+using AdventOfCode.IntCode.IO;
 using Xunit;
 
 namespace AdventOfCode2019Test
@@ -19,6 +21,80 @@ namespace AdventOfCode2019Test
             var expected = 9006327;
             var result = Day5.Part2();
             Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ExecuteInstruction1Test()
+        {
+            long[] program = new long[] { 10001, 1, 1, 1 };
+            long pc = 0;
+
+            MemoryController memoryController = new MemoryController(program);
+            var instruction01 = InstructionUtils.GetInstruction(program, 0, new ConsoleInput(), new ConsoleOutput(), memoryController);
+            instruction01?.Execute(program, ref pc);
+
+            Assert.Equal(2, program[3]);
+        }
+
+        [Fact]
+        public void ExecuteInstruction2Test()
+        {
+            long[] program = { 1002, 4, 3, 4, 33 };
+            long[] expected = { 1002, 4, 3, 4, 99 };
+            MemoryController memoryController = new MemoryController(program);
+            long pc = 0;
+            var instruction01 = InstructionUtils.GetInstruction(program, 0, new ConsoleInput(), new ConsoleOutput(), memoryController);
+            instruction01?.Execute(program, ref pc);
+
+            Assert.Equal(expected, program);
+        }
+
+
+        [Fact]
+        public void ExecuteInstruction3Test()
+        {
+            long[] program = { 10002, 2, 2, 1 };
+            MemoryController memoryController = new MemoryController(program);
+            long pc = 0;
+            var instruction02 = InstructionUtils.GetInstruction(program, 0, new ConsoleInput(), new ConsoleOutput(), memoryController);
+            instruction02?.Execute(program, ref pc);
+
+            Assert.Equal(4, program[3]);
+        }
+
+        [Fact]
+        public void GetInstruction1Test()
+        {
+            long[] program = { 00001, 1, 1, 1 };
+            var instruction01 = InstructionUtils.GetInstruction(program, 0, new ConsoleInput(), new ConsoleOutput(), null) as Instruction01;
+            Assert.NotNull(instruction01);
+        }
+
+        [Fact]
+        public void GetInstruction2Test()
+        {
+            long[] program = { 00002, 1, 1, 1 };
+            var instruction02 = InstructionUtils.GetInstruction(program, 0, new ConsoleInput(), new ConsoleOutput(), null) as Instruction02;
+            Assert.NotNull(instruction02);
+
+        }
+
+        [Fact]
+        public void GetInstruction3Test()
+        {
+            long[] program = { 00003, 50, 0 };
+            var instruction03 = InstructionUtils.GetInstruction(program, 0, new ConsoleInput(), new ConsoleOutput(), null) as Instruction03;
+            Assert.NotNull(instruction03);
+
+        }
+
+        [Fact]
+        public void GetInstruction4Test()
+        {
+            long[] program = { 00004, 50, 0 };
+            var instruction03 = InstructionUtils.GetInstruction(program, 0, new ConsoleInput(), new ConsoleOutput(), null) as Instruction04;
+            Assert.NotNull(instruction03);
+
         }
     }
 }
